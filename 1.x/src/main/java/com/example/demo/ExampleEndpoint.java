f0575c61b3640edbbfff2a23cefbc239c339bbb3
Madhura Bhave <mbhave@pivotal.io>
2017-09-06 08:34:07 -0700

    Add write method to 1.x

--- 1.x/src/main/java/com/example/demo/ExampleEndpoint.java
+++ 1.x/src/main/java/com/example/demo/ExampleEndpoint.java
@@ -1,11 +1,16 @@
 package com.example.demo;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
 
 /**
  * @author Madhura Bhave
  */
 public class ExampleEndpoint extends AbstractEndpoint<String> {
+
+	private static final Log logger = LogFactory.getLog(ExampleEndpoint.class);
 
 	public ExampleEndpoint(String id) {
 		super(id, false);
@@ -14,6 +19,10 @@ public class ExampleEndpoint extends AbstractEndpoint<String> {
 	@Override
 	public String invoke() {
 		return "hello world";
+	}
+
+	public void setSomeValue(String value) {
+		logger.info("set value " + value);
 	}
 
 }
